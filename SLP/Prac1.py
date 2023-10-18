af7a105e39caab13fcaf173a5c722be1beec03d6
Kavan Bhavsar <kvbhavsar35@gmail.com>
2023-10-18 09:31:57 +0530

    Organising

---
+++ SLP/Prac1.py
@@ -0,0 +1,10 @@
+name = input("Enter you name : ")
+contNo = int(input("Enter your contact number : "))
+email = input("Enter your email id : ")
+bd = input("Enter your date of birth : ")
+
+
+print("Name: ",name)
+print("Contact Number: ",contNo)
+print("Email: ",email)
+print("Birth date: ",bd)
